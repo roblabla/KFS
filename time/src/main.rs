eacd35081faa39c298a1aaee97b9eae6e1d23975
Thomas Guillemard <me@thog.eu>
2019-06-04 13:45:28 +0200

    Start looking into the RTC interface

--- time/src/main.rs
+++ time/src/main.rs
@@ -33,8 +33,10 @@ use alloc::prelude::v1::*;
 use sunrise_libuser::syscalls;
 use sunrise_libuser::ipc::server::{WaitableManager, PortHandler, IWaitable, SessionWrapper};
 use sunrise_libuser::types::*;
+use sunrise_libuser::io::{self, Io};
 use sunrise_libuser::error::Error;
-
+use sunrise_libutils::initialize_to_zero;
+use spin::Mutex;
 
 use timezone::TimeZoneService;
 
@@ -81,6 +83,91 @@ object! {
     }
 }
 
+/// IBM Real Time Clock provides access to the current date and time (at second
+/// precision). The Real Time Clock is actually part of the CMOS on
+/// usual IBM/PC setups.
+///
+/// It is comprised of a command register and a data register. To access data
+/// store on the CMOS, one should first write the data address in the command
+/// register, then either read or write the data register to read/write to that
+/// data address. This is implemented and abstracted away by [Rtc::read_reg] and
+/// [Rtc::write_reg].
+struct Rtc {
+    /// Command Register.
+    command: io::Pio<u8>,
+    /// Data Register.
+    data: io::Pio<u8>
+}
+
+impl Rtc {
+    /// Create a new RTC with the default IBM PC values.
+    pub fn new() -> Rtc {
+        Rtc {
+            command: io::Pio::new(0x70),
+            data: io::Pio::new(0x71)
+        }
+    }
+
+    /// Read from a CMOS register.
+    fn read_reg(&mut self, reg: u8) -> u8 {
+        self.command.write(reg);
+        self.data.read()
+    }
+
+    /// Write to the CMOS register.
+    fn write_reg(&mut self, reg: u8, val: u8) {
+        self.command.write(reg);
+        self.data.write(val)
+    }
+
+    /// Enable the Update Ended RTC interrupt. This will enable an interruption
+    /// on IRQ 8 that will be thrown when the RTC is finished updating its
+    /// registers.
+    pub fn enable_update_ended_int(&mut self) {
+        // Set the rate to be as slow as possible...
+        //let oldval = self.read_reg(0xA);
+        //self.write_reg(0xA, (oldval & 0xF0) | 0xF);
+        let oldval = self.read_reg(0xB);
+        self.write_reg(0xB, oldval | (1 << 4));
+    }
+
+    /// Acknowledges an interrupt from the RTC. Necessary to receive further
+    /// interrupts.
+    pub fn read_interrupt_kind(&mut self) -> u8 {
+        self.read_reg(0xC)
+    }
+
+    /// Checks if the RTC is in 12 hours or 24 hours mode. Depending on the mode,
+    /// the date might be encoded in BCD.
+    #[allow(clippy::wrong_self_convention)] // More readable this way.
+    pub fn is_12hr_clock(&mut self) -> bool {
+        self.read_reg(0xB) & (1 << 2) != 0
+    }
+}
+
+/// Global instance of Rtc
+static RTC_INSTANCE: Mutex<Rtc> = Mutex::new(unsafe { initialize_to_zero!(Rtc) });
+
+/// RTC interface.
+#[derive(Default, Debug)]
+struct RTCManager {
+    /// Last RTC time value.
+    timestamp: Mutex<u64>
+}
+
+object! {
+    impl RTCManager {
+        #[cmdid(1)]
+        fn get_rtc_time(&mut self,) -> Result<(u64, ), Error> {
+            Ok((*self.timestamp.lock(), ))
+        }
+        #[cmdid(3)]
+        fn get_rtc_event(&mut self, manager: &WaitableManager,) -> Result<(Handle,), Error> {
+            Ok((Handle::new(0xDEAD_BEEF),))
+        }
+    }
+}
+
 use generic_array::GenericArray;
 use generic_array::typenum::consts::U36;
 
@@ -89,6 +176,7 @@ fn main() {
     let device_location_name: GenericArray<u8, U36> = GenericArray::clone_from_slice(b"Europe/Paris\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
     timezone::TZ_MANAGER.lock().set_device_location_name(device_location_name).unwrap();
 
+    //let irq = syscalls::create_interrupt_event(0x08, 0).unwrap();
     let man = WaitableManager::new();
     let user_handler = Box::new(PortHandler::<StaticService>::new("time:u\0").unwrap());
     let applet_handler = Box::new(PortHandler::<StaticService>::new("time:a\0").unwrap());
